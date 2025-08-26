7feb336289e5604aa06b10e1af8b6572025ad44c
mohankrishnaalavala <mohanalavala68@gmail.com>
2025-08-25 21:28:21 -0500

    style: black/flake8-compliant test

--- tests/test_detectors_slack_webhook.py
+++ tests/test_detectors_slack_webhook.py
@@ -1,8 +1,6 @@
 # Auto-generated smoke test for slack_webhook
+import detectors.slack_webhook as m
 
 
 def test_detector_import():
-    import importlib
-
-    m = importlib.import_module("detectors.slack_webhook")
     assert hasattr(m, "detect")
